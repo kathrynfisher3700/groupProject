2b4b8c491fd5eaec43aa563c0bf0983078ea2d3f
Aprecity <kathrynfisher3700@gmail.com>
2023-10-14 19:21:05 -0400

    removed jQuery, added vanilla

--- assets/js/script.js
+++ assets/js/script.js
@@ -49456,11 +49456,11 @@ $(function () {
                 let videos = data.items;
                 for (video of videos) {
                     let videoId = video.id.videoId;
-                    const videoLink = `https://www.youtube.com/embed/${videoId}`; //EMBED YOUTUBE VIDEO LINK
+                    const videoLink = "https://www.youtube.com/embed/"+videoId; //EMBED YOUTUBE VIDEO LINK
                     console.log(videoLink);
-                    let videoPlacement = $(".videoPlacement") //GRABS <IFRAME> ELEMENT
-                    videoPlacement.attr("src", ""); //REMOVES CURRENT SRC
-                    videoPlacement.attr("src", videoLink);  //GET VIDEOLINK INTO <IFRAME> HTML
+                    let videoPlacement = document.querySelector(".videoPlacement"); //GRABS <IFRAME> ELEMENT
+                    videoPlacement.setAttribute("src", ""); //REMOVES CURRENT SRC
+                    videoPlacement.setAttribute("src", videoLink);  //GET VIDEOLINK INTO <IFRAME> HTML
                 }
             })
     };
