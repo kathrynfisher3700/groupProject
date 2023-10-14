d797cf68a808571a862538615d67c90e6185ee0a
Aprecity <kathrynfisher3700@gmail.com>
2023-10-14 19:36:31 -0400

    adding comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -49444,8 +49444,8 @@ $(function () {
 
     let grabYoutubeVideo = function (name) {
         console.log(name);
-        let artistName = name.replace(/\s/g, '');
-        let artistSearch = artistName.toLowerCase();
+        let artistName = name.replace(/\s/g, ''); //REMOVES SPACES
+        let artistSearch = artistName.toLowerCase(); //ALL LOWERCASE
         console.log(artistSearch);
         fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${artistSearch}official&type=video&key=${apiKey}`)
             .then((result) => {
@@ -49467,8 +49467,8 @@ $(function () {
 
 
     let grabYoutubeChannel = function (name) {
-        let artistName = name.replace(/\s/g, '');
-        let channelSearch = artistName.toLowerCase();
+        let artistName = name.replace(/\s/g, ''); //REMOVES SPACES
+        let channelSearch = artistName.toLowerCase(); //ALL LOWERCASE
         fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${channelSearch}official&type=channel&chart=mostPopular&key=${apiKey}`) //need to add query that inserts artist name
             .then((result) => {
                 return result.json();
@@ -49478,16 +49478,14 @@ $(function () {
                 let videos = data.items;
                 for (video of videos) {
                     let channelId = video.id.channelId;
-                    const channelLink = "https://www.youtube.com/channel/" +channelId; //this is video link
-                    let channelPlacement = document.querySelector(".channel_link");
-                    channelPlacement.setAttribute("href", "");
-                    channelPlacement.setAttribute("href", channelLink);
+                    const channelLink = "https://www.youtube.com/channel/" +channelId; //CREATES CHANNEL LINK
+                    let channelPlacement = document.querySelector(".channel_link"); //GRABS <A> ELEMENT
+                    channelPlacement.setAttribute("href", ""); //REMOVES PREVIOUS LINK
+                    channelPlacement.setAttribute("href", channelLink); //ADDS NEW LINK
                     console.log(channelLink);
                 }
             })
     };
-    // grabYoutubeChannel();
-    // grabYoutubeVideo();
 
     // Make the token request
     // TODO: Eventually, this will need to be wrapped in a function to request a new
